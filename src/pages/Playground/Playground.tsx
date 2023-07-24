af6355e086a10db7a6ea44f554dc4694aefb1e23
twinklose <mathieu.dacheux@icloud.com>
2023-07-24 16:08:22 +0200

    FormikTextField done

--- src/pages/Playground/Playground.tsx
+++ src/pages/Playground/Playground.tsx
@@ -1,9 +1,34 @@
 import PropertyCard from '../../components/organisms/PropertyCard.tsx'
+import FormikTextField from '../../components/molecules/core/FormikTextField.tsx'
+import { FormikProvider, useFormik } from 'formik'
+import Heart from '../../components/atoms/icons/Heart.tsx'
 
 export default function Playground(): JSX.Element {
+  const initialValues = {
+    test: '',
+  }
+
+  const handleSubmit = (values: any) => {
+    console.log(values)
+  }
+
+  const form = useFormik({
+    initialValues,
+    validateOnBlur: true,
+    validateOnChange: false,
+    onSubmit: handleSubmit,
+  })
+
   return (
     <>
-      <PropertyCard />
+      <FormikProvider value={form}>
+        <FormikTextField
+          name='test'
+          placeholder='testing formik textfield'
+          icon={<Heart color />}
+        />
+        <PropertyCard />
+      </FormikProvider>
     </>
   )
 }
