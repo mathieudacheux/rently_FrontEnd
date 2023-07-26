919be08d1fe4832c452e9f6f49bda3ed287ecd44
twinklose <mathieu.dacheux@icloud.com>
2023-07-26 08:43:49 +0200

    Navbar

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,9 +1,18 @@
 import { useTranslation } from 'react-i18next'
 
-export default function Button({ text }: { text: string }): JSX.Element {
+export default function Button({
+  text,
+  onClick,
+}: {
+  text: string
+  onClick?: () => void
+}): JSX.Element {
   const { t } = useTranslation()
   return (
-    <button className='btn btn-xs sm:btn-sm md:btn-md lg:btn-md bg-primary hover:bg-primary text-white border-0'>
+    <button
+      onClick={onClick}
+      className='btn btn-xs sm:btn-sm md:btn-md lg:btn-md bg-primary hover:bg-primary text-white border-0'
+    >
       {t(text)}
     </button>
   )
