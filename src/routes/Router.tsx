0bd4de72bda9917829b18fc046aacae4fe055c06
twinklose <mathieu.dacheux@icloud.com>
2023-07-27 13:33:51 +0200

    Push

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,6 +5,7 @@ import Login from '../pages/Login/Login.tsx'
 import Register from '../pages/Register/Register.tsx'
 import Playground from '../pages/Playground/Playground.tsx'
 import Properties from '../pages/Properties/Properties.tsx'
+import ValidateAccount from '../pages/ValidateAccount/ValidateAccount.tsx'
 import { useTranslation } from 'react-i18next'
 import { useEffect } from 'react'
 
@@ -36,6 +37,7 @@ export default function RouterElement(): JSX.Element {
       <Route path={APP_ROUTES.HOME} element={<Home />} />
       <Route path={APP_ROUTES.LOGIN} element={<Login />} />
       <Route path={APP_ROUTES.REGISTER} element={<Register />} />
+      <Route path={APP_ROUTES.VALIDATE_ACCOUNT} element={<ValidateAccount />} />
       <Route path={APP_ROUTES.PROPERTIES} element={<Properties />} />
       <Route path={APP_ROUTES.PLAYGROUND} element={<Playground />} />
     </Routes>
