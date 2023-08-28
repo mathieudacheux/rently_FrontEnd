f584577b08b3169bedb04e3a4211fe9ecaa26586
twinklose <mathieu.dacheux@icloud.com>
2023-08-28 11:30:15 +0200

    Improvements

--- src/pages/Home/components/AgencySection.tsx
+++ src/pages/Home/components/AgencySection.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 import Button from '../../../components/atoms/Button.tsx'
 import { APP_ROUTES } from '../../../routes/routes.ts'
 import { useNavigate } from 'react-router-dom'
+import MapHome from './MapHome.tsx'
 
 export default function AgencySection(): JSX.Element {
   const { t } = useTranslation()
@@ -22,24 +23,20 @@ export default function AgencySection(): JSX.Element {
         md:justify-between pt-7'
       >
         <div className='h-[810px] w-full md:max-w-[50%] flex justify-center mt-[30px] md:mt-0'>
-          <BlogCard
-            title='blogCard.title'
-            link='blogCard.link'
-            description='blogCard.description'
-          />
+          <MapHome />
         </div>
         <div className='flex flex-col items-center md:items-end w-full md:1/2'>
           <div className='h-[390px] w-full md:w-[calc(100%-30px)] flex justify-center'>
             <BlogCard
+              id={1}
               title='blogCard.title'
-              link='blogCard.link'
               description='blogCard.description'
             />
           </div>
           <div className='h-[390px] w-full md:w-[calc(100%-30px)] flex justify-center mt-[30px]'>
             <BlogCard
+              id={1}
               title='blogCard.title'
-              link='blogCard.link'
               description='blogCard.description'
             />
           </div>
