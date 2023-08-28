0962756c352894feda3f319ea2445b166f71d826
twinklose <mathieu.dacheux@icloud.com>
2023-08-28 13:27:36 +0200

    Improvements

--- src/pages/MyAccount/MyAccountManagement/MyAccountManagement.tsx
+++ src/pages/MyAccount/MyAccountManagement/MyAccountManagement.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import { settingsList } from './constants/constants.ts'
 import { useTranslation } from 'react-i18next'
+import PasswordSection from './components/PasswordSection.tsx'
 
 export default function MyAccountManagement(): JSX.Element {
   const { t } = useTranslation()
-  const [activeSetting, setActiveSetting] = useState(settingsList[0])
+  const [activeSetting, setActiveSetting] = useState<number>(1)
 
   return (
-    <div className='w-full h-[calc(100vh-75px)] flex flex-row justify-between p-4'>
+    <div className='w-full h-[calc(100vh-75px)] flex flex-row p-4'>
       <div className=' w-[250px] flex flex-col items-start border-r-[1px] border-neutral-900'>
         {settingsList.map((setting) => (
           <div
@@ -15,15 +16,18 @@ export default function MyAccountManagement(): JSX.Element {
             className={`w-[234px] h-[50px] flex flex-row items-center justify-start mb-4 p-4 cursor-pointer rounded-[10px] 
               transition-all duration-300 hover:bg-primary hover:text-neutral-100
             ${
-              activeSetting === setting
+              activeSetting === setting?.id
                 ? 'bg-primary text-neutral-100'
                 : 'bg-neutral-100 text-neutral-500'
             }`}
-            onClick={() => setActiveSetting(setting)}
+            onClick={() => setActiveSetting(setting.id)}
           >
             {t(setting.name)}
           </div>
         ))}
+      </div>
+      <div className='w-[calc(100vw-250px)]'>
+        {activeSetting === 2 && <PasswordSection />}
       </div>
     </div>
   )
---
+++ src/pages/MyAccount/MyAccountManagement/components/PasswordSection.tsx
@@ -0,0 +1,70 @@
+import { useTranslation } from 'react-i18next'
+import Typography from '../../../../components/atoms/Typography.tsx'
+import FormikTextField from '../../../../components/molecules/core/FormikTextField.tsx'
+import Password from '../../../../components/atoms/icons/Password.tsx'
+import CardButton from '../../../../components/atoms/CardButton.tsx'
+
+export default function PasswordSection(): JSX.Element {
+  const { t } = useTranslation()
+
+  return (
+    <div className='pl-4 w-full'>
+      <div className='flex flex-col justify-center align-middle w-full'>
+        <Typography variant='h2' className='text-neutral-900'>
+          {t('myAccount.passwordSection.title')}
+        </Typography>
+        <div className='pt-4 w-full'>
+          <Typography variant='text' className='text-neutral-900'>
+            {t('myAccount.passwordSection.oldPassword')}
+          </Typography>
+        </div>
+        <div className='pt-4 w-2/5'>
+          <FormikTextField
+            name='passwordOne'
+            password
+            placeholder={t('connection.password')}
+            icon={<Password />}
+          />
+        </div>
+        <div className='flex flex-col md:flex-row flex-start w-full'>
+          <div className='flex flex-col w-2/5 mr-4'>
+            <div className='pt-4 w-full'>
+              <Typography variant='text' className='text-neutral-900'>
+                {t('connection.password')}
+              </Typography>
+            </div>
+            <div className='pt-2 w-full'>
+              <FormikTextField
+                name='passwordOne'
+                password
+                placeholder={t('connection.password')}
+                icon={<Password />}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col w-2/5'>
+            <div className='pt-4 w-full'>
+              <Typography variant='text' className='text-neutral-900'>
+                {t('connection.passwordConfirm')}
+              </Typography>
+            </div>
+            <div className='pt-2'>
+              <FormikTextField
+                name='passwordTwo'
+                password
+                placeholder={t('connection.password')}
+                icon={<Password />}
+              />
+            </div>
+          </div>
+        </div>
+        <div className='pt-4 w-[125px]'>
+          <CardButton
+            onClick={() => console.log('z')}
+            text='myAccount.passwordSection.button'
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
