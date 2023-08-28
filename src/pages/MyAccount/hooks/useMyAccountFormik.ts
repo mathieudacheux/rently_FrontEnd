a4b43bb71643ca369a4b486e7e3140771e2fef7a
twinklose <mathieu.dacheux@icloud.com>
2023-08-28 16:07:24 +0200

    Push

--- src/pages/MyAccount/hooks/useMyAccountFormik.ts
+++ src/pages/MyAccount/hooks/useMyAccountFormik.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useMemo } from 'react'
 import { useFormik } from 'formik'
 import * as yup from 'yup'
 import { useTranslation } from 'react-i18next'
