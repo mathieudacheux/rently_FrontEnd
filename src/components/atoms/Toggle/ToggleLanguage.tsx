345ab77bfef7ccb9e48627467f3e5dd635ff5349
twinklose <mathieu.dacheux@icloud.com>
2023-08-28 14:34:12 +0200

    ToggleImprovements

--- src/components/atoms/Toggle/ToggleLanguage.tsx
+++ src/components/atoms/Toggle/ToggleLanguage.tsx
@@ -11,8 +11,10 @@ export default function ToggleLanguage(): JSX.Element {
   }
 
   useEffect(() => {
+    if (language === window.localStorage.getItem('lang')) return
     i18next.changeLanguage(language)
     window.localStorage.setItem('lang', language)
+    window.location.reload()
   }, [language])
 
   return (
@@ -24,7 +26,7 @@ export default function ToggleLanguage(): JSX.Element {
         onClick={toggleLanguage}
         {...(language === 'fr' ? { checked: true } : {})}
       />
-      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:primary rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
+      <div className="w-11 h-6 bg-primary peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:primary rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
       <span className='ml-3 text-sm font-medium text-gray-900 dark:text-gray-300'>
         {language === 'fr' ? 'Français' : 'Anglais'}
       </span>
