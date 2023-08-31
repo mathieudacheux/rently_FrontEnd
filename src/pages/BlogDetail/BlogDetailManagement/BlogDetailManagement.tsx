025f46a48d3c647b391744db9c129d2143442a3c
twinklose <mathieu.dacheux@icloud.com>
2023-08-31 10:48:57 +0200

    Improvements

--- src/pages/BlogDetail/BlogDetailManagement/BlogDetailManagement.tsx
+++ src/pages/BlogDetail/BlogDetailManagement/BlogDetailManagement.tsx
@@ -1,5 +1,5 @@
 import { ArticleSerializerRead } from '../../../api/index.ts'
-import ReactHTMLParser from 'react-html-parser'
+import HTMLReactParser from 'html-react-parser'
 
 export default function BlogDetailManagement({
   articleData,
@@ -9,47 +9,7 @@ export default function BlogDetailManagement({
   const renderContent = (content: string) => {
     const description = content.replace(/\\/g, '')
 
-    return (
-      <div>
-        {...ReactHTMLParser(description, {
-          transform: (node) => {
-            if (node.name === 'img') {
-              return (
-                <img
-                  className='w-full h-auto mb-4'
-                  src={node.attribs.src}
-                  alt={node.attribs.alt}
-                />
-              )
-            } else if (node.name === 'h2') {
-              return (
-                <h1 className='text-2xl font-bold text-primary mb-4'>
-                  {node.children[0].data}
-                </h1>
-              )
-            } else if (node.name === 'h3') {
-              return (
-                <h2 className='text-xl font-bold text-neutral-900 my-2'>
-                  {node.children[0].data}
-                </h2>
-              )
-            } else if (node.name === 'h4') {
-              return (
-                <h3 className='text-lg font-bold text-neutral-900 my-2'>
-                  {node.children[0].data}
-                </h3>
-              )
-            } else if (node.name === 'p') {
-              return (
-                <p className='text-sm text-neutral-900'>
-                  {node.children[0].data}
-                </p>
-              )
-            }
-          },
-        })}
-      </div>
-    )
+    return <div className='w-full'>{HTMLReactParser(description)}</div>
   }
 
   return (
