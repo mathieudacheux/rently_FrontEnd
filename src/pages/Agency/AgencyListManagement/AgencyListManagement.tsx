7f28d2ec2a5a46ea1c9f1d30d127cc1c9ed04e26
twinklose <mathieu.dacheux@icloud.com>
2023-09-27 14:38:21 +0200

    Adding new translations

--- src/pages/Agency/AgencyListManagement/AgencyListManagement.tsx
+++ src/pages/Agency/AgencyListManagement/AgencyListManagement.tsx
@@ -1,13 +1,16 @@
 import BlogCard from '../../../components/organisms/BlogCard.tsx'
 import Typography from '../../../components/atoms/Typography.tsx'
 import { useGetAgenciesQuery } from '../../../features/agency/agencyApi.ts'
+import { useTranslation } from 'react-i18next'
 
 export default function AgencyListManagement(): JSX.Element {
+  const { t } = useTranslation()
   const { data: agencies } = useGetAgenciesQuery({})
+
   return (
     <div className='w-full flex flex-col items-center'>
       <div className='w-11/12 flex justify-center items-center mb-7'>
-        <Typography variant='h1'>Agency List Management</Typography>
+        <Typography variant='h1'>{t('agency.home')}</Typography>
       </div>
       <div className='w-11/12 flex flex-col md:flex-row flex-wrap items-center justify-start md:items-start md:justify-between gap-5 md:gap-10'>
         {agencies?.map((agency: any) => (
