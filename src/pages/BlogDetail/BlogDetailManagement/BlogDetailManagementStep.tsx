f03c5bc0efaecf1a1f7e5ce849916072fb12d3f1
twinklose <mathieu.dacheux@icloud.com>
2023-09-27 10:44:59 +0200

    Improvements on fetch for Blog

--- src/pages/BlogDetail/BlogDetailManagement/BlogDetailManagementStep.tsx
+++ src/pages/BlogDetail/BlogDetailManagement/BlogDetailManagementStep.tsx
@@ -4,10 +4,7 @@ import { useGetArticleByIdQuery } from '../../../features/article/articleApi.ts'
 
 export default function BlogDetailManagementStep(): JSX.Element {
   const { id } = useParams<{ id: string }>()
-  console.log('id', id)
   const articleData = useGetArticleByIdQuery(Number(id))?.data
-
-  console.log('articleData', articleData)
 
   return <BlogDetailManagement articleData={articleData} />
 }
